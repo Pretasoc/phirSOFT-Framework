9218009c056883feb9aafb20a3834ebbee5de27a
Philemon Eichin <philemon@phirSOFT.invalid>
2016-09-30 12:46:40 +0200

    Created phirSOFT.Common Project

---
+++ phirSOFT.Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("phirSOFT.Common")]
+[assembly: AssemblyDescription("This assembly contains common used functions and classes.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCulture("en")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7b991f9e-665f-46f8-a84f-971d603bc24e")]
+[assembly: NeutralResourcesLanguage("en")]
+
